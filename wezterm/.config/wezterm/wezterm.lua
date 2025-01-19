cb6f7982e05d7d808971738887db6c8270a3fce8
Vincent Fournier <v.fournier11@gmail.com>
2025-01-19 12:34:25 -0500

    consistent color schemes in nvim and wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,16 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.color_scheme = 'Palenight (Gogh)'
+
 config.font_size = 18
+
+config.use_fancy_tab_bar = false
+
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.55,
+}
 
 return config
 
