cc4b7bd65caf184967f6c072f1b9b7bf3efa1103
Vincent Fournier <v.fournier11@gmail.com>
2025-01-19 10:27:21 -0500

    Add partial fish configuration

---
+++ fish/.config/fish/shared.fish
@@ -0,0 +1,8 @@
+# translation - requires https://www.soimort.org/translate-shell/
+abbr -a -- tes trans es:fr
+abbr -a -- tfr trans fr:es
+abbr -a -- ten trans en:es
+
+# starship
+starship init fish | source
+
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 stow stow \
+  fish \
 	nvim
+
+echo "Add 'source ~/.config/fish/shared.fish' to your fish configuration."
