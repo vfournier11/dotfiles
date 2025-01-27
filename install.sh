1e22a62ca1c0a302d583ef0b73fb4b36a3741a48
Vincent Fournier <vincent.fournier@ubisoft.com>
2025-01-27 12:19:10 -0500

    Add Wezterm installation for Windows

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(dirname "$0")
+
 stow stow \
   fish \
   nvim \
   wezterm
+
+WINDOWS_FOLDER=/mnt/c/Users/vfournier
+if test -e "$WINDOWS_FOLDER"; then
+  echo "$WINDOWS_FOLDER found."
+  echo "  updating Wezterm"
+  rm -f $WINDOWS_FOLDER/.wezterm.lua # use `-f` to fail silently.
+  cp $SCRIPT_DIR/wezterm/.config/wezterm/wezterm.lua /mnt/c/Users/vfournier/.wezterm.lua
+fi
 
 echo "Add 'source ~/.config/fish/shared.fish' to your fish configuration."
