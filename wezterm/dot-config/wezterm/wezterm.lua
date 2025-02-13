76d90ba2cc1d4eca3c334d0faeaea633be583600
Vincent Fournier <vincent.fournier@ubisoft.com>
2025-02-13 09:56:24 -0500

    Update split keyboard shortcuts

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -4,13 +4,28 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Palenight (Gogh)'
 
-config.font_size = 14
+config.font = wezterm.font 'Iosevka Nerd Font'
+config.font_size = 12
 
 config.use_fancy_tab_bar = false
 
 config.inactive_pane_hsb = {
   saturation = 0.8,
   brightness = 0.55,
+}
+
+local act = wezterm.action
+config.keys = {
+  {
+    key = "v",
+    mods = "CTRL|SHIFT|ALT",
+    action = act.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "h",
+    mods = "CTRL|SHIFT|ALT",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
 }
 
 return config
